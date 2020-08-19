76a19f3a3e0c549f09a1a6f95b7f524efa20843b
Falk <julien.delusseau@gmail.com>
2020-08-19 12:29:48 +0200

    First commit

---
+++ data/workDB.js
@@ -0,0 +1,34 @@
+const workDB = [
+  {
+    id: 3,
+    tags: ['HTML', 'JSS', 'React', 'Redux', 'Redux-Persist', 'Firebase'],
+    titre: 'Amazon Clone',
+    img: '/assets/images/project06.png',
+    description:
+      'Clone du célèbre site Amazon, créé avec React, Redux et Firebase.Vous pouvez vous identifier, ajouter et supprimer des items de votre panier, et conserver vos données.',
+    link: 'https://elated-wozniak-b4d131.netlify.app',
+    github: 'https://github.com/Djuls69/amazon-clone'
+  },
+  {
+    id: 2,
+    tags: ['HTML', 'SASS', 'Javascript', 'React', 'Redux', 'Firebase'],
+    titre: 'CRWN Clothing',
+    img: '/assets/images/project05.png',
+    description:
+      "Développement d'une e-commerce, à l'aide de React et Redux, utilisant Firebase pour la base de données et l'authentification, et intégrant multiples librairies.",
+    link: 'https://react-crwnclothing-app.herokuapp.com',
+    github: 'https://github.com/Djuls69/crwn-clothing'
+  },
+  {
+    id: 1,
+    tags: ['HTML', 'CSS', 'Javascript', 'React', 'Material UI'],
+    titre: 'Trafalgar',
+    img: '/assets/images/project04.png',
+    description:
+      "Développement d'une landing page, exclusivement en React, à partir d'une maquette réalisé par un Web Designer professionnel.",
+    link: 'https://hungry-goodall-0f9d3e.netlify.app',
+    github: 'https://github.com/Djuls69/Trafalgar'
+  }
+]
+
+export default workDB
