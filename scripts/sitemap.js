347415db46256cfe032cab38a76b144cfa7f8058
Falk <julien.delusseau@gmail.com>
2020-08-19 15:57:40 +0200

    Some minors changes

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -16,7 +16,7 @@ const today = formatDate(new Date())
 const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${Object.keys(pathsObj)
-    .filter(path => path !== '/index' && path !== '/_app')
+    .filter(path => path !== '/_app')
     .map(
       path => `<url>
     ${
